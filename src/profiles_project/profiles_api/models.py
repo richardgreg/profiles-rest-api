6ea69d8c87bd520ff92db772ab479dc7411cadbb
Richard O. Gregory <richardgrecoson@gmail.com>
2019-03-19 18:28:02 +0100

    Create user profile model.
    This is a custom model that helps manage users like creating a new user, checking credentials and deleting a user.

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
 # Create your models here.
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """
+    Represents a 'user profile' inside the system.
+    Stores all user account related data, such as 'email address' and 'name'
+    """
+
+    email = models.EmailFied(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Django uses this when it needs to get the user's full name."""
+
+        return self.name
+
+    def get_short_name(self):
+        """Django uses this when it needs to get the users abbreviated name."""
+
+        return self.name
+
+    def __str__(self):
+        """Django uses this when it needs to convert the object to text."""
+
+        return self.email
