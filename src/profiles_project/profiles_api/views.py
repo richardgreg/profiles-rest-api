32701bef4ebc13cb5c9ffaca537fbb16da14a37e
Richard O. Gregory <richardgrecoson@gmail.com>
2019-04-01 18:18:27 +0100

    Add POST to APIView.
    This includes the applying the django rest-framework serializer class and hadling errors with the rest-framwork built-in status codes.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
 
 # Create your views here.
 class HelloAPIView(APIView):
     '''Test API View'''
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         '''Returns a list of API view functions'''
@@ -17,3 +22,16 @@ class HelloAPIView(APIView):
         ]
 
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        '''Create a hello message with name'''
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        # Retrieve the data (name) if valid.
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
