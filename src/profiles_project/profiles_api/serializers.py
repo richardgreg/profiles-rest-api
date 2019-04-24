fbf99565d01f7989f7c121a830e9e3c51cc642b8
Richard O. Gregory <richardgrecoson@gmail.com>
2019-04-24 18:13:54 +0100

    Create user profile serializer. The django rest framework serializer makes use of the user-profile model to validate and create new users.

--- src/profiles_project/profiles_api/serializers.py
+++ src/profiles_project/profiles_api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from . import models
 
 
 class HelloSerializer(serializers.Serializer):
@@ -6,3 +7,25 @@ class HelloSerializer(serializers.Serializer):
 
     name = serializers.CharField(max_length=10)
 
+class UserProfileSerializer(serializers.ModelSerializer):
+    """A serializer for user profile object"""
+
+    # Specifies what classes we want from models
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'name', 'email', 'password')
+        # Extra keyword argument
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        """Create and return a new user"""
+
+        user = models.UserProfile(
+            email = validated_data['email']
+            name = validated_data['name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
